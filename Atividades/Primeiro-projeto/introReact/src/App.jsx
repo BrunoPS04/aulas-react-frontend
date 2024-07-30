cbb7bce0131be5ca8e042a1ff21d7bd2f9d69e32
BRUNO PEREIRA SEVERINO <bruno_p_severino@estudante.sesisenai.org.br>
2024-07-30 17:12:30 -0300

    primeiro projeto react

---
+++ Atividades/Primeiro-projeto/introReact/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Conteudo from "./components/Conteudo";
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  return (
+
+    <>
+
+      <Navbar />
+      <Conteudo />
+
+    </>
+
+  );
+
+}
+
+export default App;
---
+++ Atividades/Primeiro-projeto/introReact/src/components/Conteudo/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.css'
+
+function Conteudo() {
+  return (
+    <div className="conteudo-container">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum porro
+        necessitatibus eius sed voluptas, vel at labore est officia ad fuga
+        deleniti, id, dolores facere ab mollitia ipsam totam incidunt!
+      </p>
+    </div>
+  );
+}
+
+export default Conteudo;
